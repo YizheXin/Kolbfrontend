ba7ce4089341a03da434db8ebce920a982cb035e
sagaofdemon <sagaofdemon@gmail.com>
2024-12-04 13:23:54 +1100

    reimplement file grid page

--- app/[folderName]/page.tsx
+++ app/[folderName]/page.tsx
@@ -10,7 +10,6 @@ export default async function BucketPage({
   // URL decode the bucket name
   const decodedFolderName = decodeURIComponent(folderName);
   const { data } = await StorageOperations.fetchFiles(decodedFolderName);
-  console.log("checking data on file grid",data.files.slice(-10))
   if (!data) {
     notFound();
   }
